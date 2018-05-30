5a4d1c54bc68a341424a397d27e186e26e64570e
Dominic Azaris <dominic.azaris@nyriad.com>
2018-05-30 16:57:47 +1200

    function documentation

--- src/assignment3/core.clj
+++ src/assignment3/core.clj
@@ -85,12 +85,16 @@
   (set-rgb image x y [grey grey grey])
 )
 
-(defn get-val-slow [image x y]
+(defn get-val-slow 
+  "Gets the average value from RGB"
+  [image x y]
   (int (/ (reduce + (get-rgb image x y) ) 3.0))
 )
 (def get-val (memoize get-val-slow))
 
-(defn apply-filter-slow [image output x y i]
+(defn apply-filter-slow 
+  "Apllies the given filter i to the image at [x, y]"
+  [image output x y i]
   (let [iwidth  (- (get-width image) 1)
         iheight (- (get-height image) 1)]
   (def new_val 0)
@@ -116,7 +120,9 @@
 )
 (def apply-filter (memoize apply-filter-slow))
 
-(defn kirsh-slow [file i] 
+(defn kirsh-slow 
+  "Applies the given kirsh filter i to the input image"
+  [file i]
   (let  [ 
           input (read-image file)
           iwidth (get-width input)
@@ -133,17 +139,23 @@
 )
 (def kirsh (memoize kirsh-slow))
 
-(defn apply-all-kirsh-slow [file]
+(defn apply-all-kirsh-slow 
+  "Applies all the kirsh filters to the given image"
+  [file]
   (doall (map #(kirsh file %) (range 8)))
 )
 (def apply-all-kirsh (memoize apply-all-kirsh-slow))
 
-(defn normalize-slow [histogram]
+(defn normalize-slow 
+  "Normalizes a given histogram"
+  [histogram]
   (vec (doall (map #(double(/ % (reduce + histogram))) histogram)))
 )
 (def normalize (memoize normalize-slow))
 
-(defn bin-image-slow [image width height n_bins]
+(defn bin-image-slow 
+  "Takes the vales of each pixel in an image, and sorts them in to n_bins bins"
+  [image width height n_bins]
   (let  [
           bin_size (int (/ 256 n_bins))
         ]
@@ -158,34 +170,40 @@
 )
 (def bin-image (memoize bin-image-slow))
 
-(defn edge-magnitude-hist-slow [file]
-    (let [
-            images    (apply-all-kirsh file)
-            first_img (nth images 0)
-            iwidth    (get-width first_img)
-            iheight   (get-height first_img)
-            mag       (new-image iwidth iheight)
-         ]
+(defn edge-magnitude-hist-slow 
+  "Returns a normalized histogram of the edge magnitute of image file"
+  [file]
+  (let [
+          images    (apply-all-kirsh file)
+          first_img (nth images 0)
+          iwidth    (get-width first_img)
+          iheight   (get-height first_img)
+          mag       (new-image iwidth iheight)
+        ]
 
-          ;; combine each image to get the magnitude image
-          (dotimes [x iwidth]
-            (dotimes [y iheight]
-              (set-grey mag x y (apply max (doall (for [img images] (get-val img x y)))))
-            )
+        ;; combine each image to get the magnitude image
+        (dotimes [x iwidth]
+          (dotimes [y iheight]
+            (set-grey mag x y (apply max (doall (for [img images] (get-val img x y)))))
           )
+        )
 
-          ;; Sort into 8 different bins & normalize
-          (normalize (bin-image mag iwidth iheight 8))
-    )
+        ;; Sort into 8 different bins & normalize
+        (normalize (bin-image mag iwidth iheight 8))
+  )
 )
 (def edge-magnitude-hist (memoize edge-magnitude-hist-slow))
 
-(defn get-index-slow [item items]
+(defn get-index-slow 
+  "Gets the indexes of any value in items that matches `item`"
+  [item items]
   (keep-indexed #(when (= %2 item) %1) items)
 )
 (def get-index (memoize get-index-slow))
 
-(defn argmax-slow [images x y]
+(defn argmax-slow 
+  "Gets the argmax of the given image at x,y"
+  [images x y]
   (let  [
           image_values (vec (doall (for [img images] (get-val img x y))))
         ]
@@ -194,41 +212,47 @@
 )
 (def argmax (memoize argmax-slow))
 
-(defn edge-direction-hist-slow [file]
-    (let [
-            images    (apply-all-kirsh file)
-            first_img (nth images 0)
-            iwidth    (get-width first_img)
-            iheight   (get-height first_img)
-            dir       (new-image iwidth iheight)
-         ]
+(defn edge-direction-hist-slow 
+  "Returns a normalized histogram of the edge direction of image file"
+  [file]
+  (let [
+          images    (apply-all-kirsh file)
+          first_img (nth images 0)
+          iwidth    (get-width first_img)
+          iheight   (get-height first_img)
+          dir       (new-image iwidth iheight)
+        ]
 
-          ;; combine each image to get the direction image
-          (def bins (vec (make-array Integer/TYPE 8)))
-          (dotimes [x iwidth]
-            (dotimes [y iheight]
-              (def bins (assoc bins (argmax images x y) (inc (nth bins (argmax images x y)))))
-            )
+        ;; combine each image to get the direction image
+        (def bins (vec (make-array Integer/TYPE 8)))
+        (dotimes [x iwidth]
+          (dotimes [y iheight]
+            (def bins (assoc bins (argmax images x y) (inc (nth bins (argmax images x y)))))
           )
+        )
 
-          ;; Sort into 8 different bins & normalize
-          (normalize bins)
-    )
+        ;; Sort into 8 different bins & normalize
+        (normalize bins)
+  )
 )
 (def edge-direction-hist (memoize edge-direction-hist-slow))
 
-(defn intensity-slow [file]
-    (let [
-            image     (read-image file)
-            iwidth    (get-width image)
-            iheight   (get-height image)
-         ]
-         (normalize (bin-image image iwidth iheight 8))
-    )
+(defn intensity-slow 
+  "Returns the intensity histogram for the given image file"
+  [file]
+  (let [
+          image     (read-image file)
+          iwidth    (get-width image)
+          iheight   (get-height image)
+        ]
+        (normalize (bin-image image iwidth iheight 8))
+  )
 )
 (def intensity (memoize intensity-slow))
 
-(defn image-descriptor-slow [file]
+(defn image-descriptor-slow 
+  "Returns the image descriptor for the given image"
+  [file]
   (let [
     edge_dir  (vec (edge-direction-hist file))
     edge_mag  (vec (edge-magnitude-hist file))
@@ -239,7 +263,9 @@
 )
 (def image-descriptor (memoize image-descriptor-slow))
 
-(defn image-similarity-slow [file1 file2]
+(defn image-similarity-slow 
+  "Returns a number between 0->1 which describes how similar two image files are"
+  [file1 file2]
   (let  [
           image1 (image-descriptor file1)
           image2 (image-descriptor file2)
@@ -250,8 +276,8 @@
 (def image-similarity (memoize image-similarity-slow))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Takes 2 images and outputs the similarity"
   [& args]
-  (println "Files:" (first args) (second args))
+  (println "Files:     " (first args) (second args))
   (println "Similarity:" (image-similarity (first args) (second args)))
 )
